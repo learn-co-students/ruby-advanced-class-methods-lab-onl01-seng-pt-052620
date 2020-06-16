8e6c92c5603a1d5d9f4a1865e1bde24d9cf5fbc0
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-16 16:06:35 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,28 @@
 class Song
   attr_accessor :name, :artist_name
+  
   @@all = []
 
+  def self.create
+    @@all << self.new
+    @@all[0]
+  end
+  
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+  end
+  
+  def self.new_by_name(song_name)
+    self.new.name = song_name
+    self.new
+  end
+  
+  def self.create_by_name
+    
   end
 
 end
